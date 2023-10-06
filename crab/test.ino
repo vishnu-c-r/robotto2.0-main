3a3e935c10cd92898bf434d4666650ee04c08481
Vishnu C Rajeev <66208248+vishnu-c-r@users.noreply.github.com>
2023-10-06 11:40:45 +0530

    Create test.ino

---
+++ crab/test.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600
+#define SERVO_FREQ 50
+
+int servonum = 8;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8 channel Servo test!");
+
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);  
+
+  delay(10);
+}
+
+
+void loop() {
+  // Drive each servo one at a time using setPWM()
+  Serial.println(servonum);
+  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+    pwm.setPWM(servonum, 0, pulselen);
+  }
+
+  delay(500);
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    pwm.setPWM(servonum, 0, pulselen);
+  }
+
+  delay(500);
+   servonum++;
+  if (servonum > 15) servonum = 8;
+}
